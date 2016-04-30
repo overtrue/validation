a2310030b2054ec17c272b0fd79ca47671f525a7
overtrue <anzhengchao@gmail.com>
2016-04-30 12:31:42 +0800

    Fix #1

--- src/helpers.php
+++ src/helpers.php
@@ -12,95 +12,119 @@
 namespace Overtrue\Validation;
 
 use ArrayAccess;
+use Closure;
 
-/**
- * Return array specific item.
- *
- * @param array  $array
- * @param string $key
- * @param mixed  $default
- *
- * @return mixed
- */
-function array_get($array, $key, $default = null)
-{
-    if (!array_accessible($array)) {
-        return value($default);
-    }
-
-    if (is_null($key)) {
-        return $array;
-    }
-
-    if (array_exists($array, $key)) {
-        return $array[$key];
-    }
-
-    foreach (explode('.', $key) as $segment) {
-        if (array_accessible($array) && array_exists($array, $segment)) {
-            $array = $array[$segment];
-        } else {
+if (! function_exists('array_get')) {
+    /**
+     * Return array specific item.
+     *
+     * @param array  $array
+     * @param string $key
+     * @param mixed  $default
+     *
+     * @return mixed
+     */
+    function array_get($array, $key, $default = null)
+    {
+        if (!array_accessible($array)) {
             return value($default);
         }
+
+        if (is_null($key)) {
+            return $array;
+        }
+
+        if (array_exists($array, $key)) {
+            return $array[$key];
+        }
+
+        foreach (explode('.', $key) as $segment) {
+            if (array_accessible($array) && array_exists($array, $segment)) {
+                $array = $array[$segment];
+            } else {
+                return value($default);
+            }
+        }
+
+        return $array;
     }
-
-    return $array;
 }
 
-/**
- * Check input is array accessable.
- *
- * @param mixed $value
- *
- * @return bool
- */
-function array_accessible($value)
-{
-    return is_array($value) || $value instanceof ArrayAccess;
-}
-
-/**
- * Check array key exists.
- *
- * @param array  $array
- * @param string $key
- *
- * @return bool
- */
-function array_exists($array, $key)
-{
-    if ($array instanceof ArrayAccess) {
-        return $array->offsetExists($key);
+if (! function_exists('array_accessible')) {
+    /**
+     * Check input is array accessable.
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    function array_accessible($value)
+    {
+        return is_array($value) || $value instanceof ArrayAccess;
     }
-
-    return array_key_exists($key, $array);
 }
 
-/**
- * Convert a string to snake case.
- *
- * @param string $string
- * @param string $delimiter
- *
- * @return string
- */
-function snake_case($string, $delimiter = '_')
-{
-    $replace = '$1'.$delimiter.'$2';
+if (! function_exists('array_exists')) {
+    /**
+     * Check array key exists.
+     *
+     * @param array  $array
+     * @param string $key
+     *
+     * @return bool
+     */
+    function array_exists($array, $key)
+    {
+        if ($array instanceof ArrayAccess) {
+            return $array->offsetExists($key);
+        }
 
-    return ctype_lower($string) ? $string : strtolower(preg_replace('/(.)([A-Z])/', $replace, $string));
+        return array_key_exists($key, $array);
+    }
 }
 
-/**
- * Convert a value to studly caps case.
- *
- * @param string $string
- *
- * @return string
- */
-function studly_case($string)
-{
-    $string = ucwords(str_replace(['-', '_'], ' ', $string));
+if (! function_exists('snake_case')) {
+    /**
+     * Convert a string to snake case.
+     *
+     * @param string $string
+     * @param string $delimiter
+     *
+     * @return string
+     */
+    function snake_case($string, $delimiter = '_')
+    {
+        $replace = '$1'.$delimiter.'$2';
 
-    return str_replace(' ', '', $string);
+        return ctype_lower($string) ? $string : strtolower(preg_replace('/(.)([A-Z])/', $replace, $string));
+    }
+}
+
+if (! function_exists('studly_case')) {
+    /**
+     * Convert a value to studly caps case.
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    function studly_case($string)
+    {
+        $string = ucwords(str_replace(['-', '_'], ' ', $string));
+
+        return str_replace(' ', '', $string);
+    }
+}
+
+if (! function_exists('value')) {
+    /**
+     * Return the default value of the given value.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    function value($value)
+    {
+        return $value instanceof Closure ? $value() : $value;
+    }
 }
